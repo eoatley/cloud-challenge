55913e0cc5f567f131d46cb319e3529ac901f204
Emma <emma.mccracken@sainsburys.co.uk>
2024-06-17 08:29:22 +0100

    change returned body from integer to json

--- lambda-function/test_lambda_function.py
+++ lambda-function/test_lambda_function.py
@@ -6,6 +6,7 @@ os.environ['AWS_SESSION_TOKEN'] = 'testing'
 os.environ['AWS_DEFAULT_REGION'] = 'us-west-2'
 import boto3
 from moto import mock_aws
+import json
 from lambda_function import lambda_handler
 
 
@@ -20,11 +21,14 @@ def test_lambda_handler():
         AttributeDefinitions=[{'AttributeName': 'emma-cv', 'AttributeType': 'S'}],
         ProvisionedThroughput={'ReadCapacityUnits': 5, 'WriteCapacityUnits': 5}
     )
-    table.put_item(Item={'emma-cv': 'emma-cv', 'visitors': '0'})
+    table.put_item(Item={'emma-cv': 'emma-cv', 'visitors': 10})
 
     # Call the lambda function
     response = lambda_handler({}, {})
 
+    # Parse the JSON string into a dictionary
+    body = json.loads(response['body'])
+
     # Check the response
     assert response['statusCode'] == 200
-    assert response['body'] == 1
+    assert body['new_visitors'] == 11
